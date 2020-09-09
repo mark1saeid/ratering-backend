bdecd6a7d96821e10854277a0b67ef8df838c0dd
Your Name <you@example.com>
2020-09-09 20:23:54 +0200

    hh

--- config/database.php
+++ config/database.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('postgres://uopnkuihlrhego:3a25223f1d62addcc62d4e9ad1d7ce4acc1a129c553e74cd6a89696e83d04a5c@ec2-54-160-202-3.compute-1.amazonaws.com:5432/d2dprbcoevdh1k');
 return [
 
     /*
@@ -15,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +67,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL(["host"]),
+            'port' => $DATABASE_URL(["port"]),
+            'database' => ltrim($DATABASE_URL["patch"],"/"),
+            'username' => $DATABASE_URL(["user"]),
+            'password' => $DATABASE_URL(["pass"]),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
