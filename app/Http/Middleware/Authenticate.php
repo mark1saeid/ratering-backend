76992edda54d1e5efd78feeaf29142b911b8d637
Your Name <you@example.com>
2020-09-24 09:50:08 +0200

    ss

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,11 +3,7 @@
 namespace App\Http\Middleware;
 
 
-use Closure;
-use http\Exception;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use Tymon\JWTAuth\Exceptions\JWTException;
-use Tymon\JWTAuth\JWTAuth;
 
 
 class Authenticate extends Middleware
@@ -18,21 +14,13 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-    public function handle($request, Closure $next)
-    {
-        try {
-            $user = (new \Tymon\JWTAuth\JWTAuth)->parseToken()->authenticate();
-        } catch (Exception $e) {
-            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
-                return response()->json(['status' => 'Token is Invalid']);
-            }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-                return response()->json(['status' => 'Token is Expired']);
-            }else{
-                return response()->json(['status' => 'Authorization Token not found']);
-            }
-        } catch (JWTException $e) {
-        }
-        return $next($request);
+   protected function redirectTo($request)
+   {
+       if (! $request->expectsJson()) {
+          return route('login');
+
+       }
+
     }
 
 
