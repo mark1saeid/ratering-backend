1d9e57bb51c8680bf592bb996164a0cdcbc273ff
Your Name <you@example.com>
2020-09-24 09:09:55 +0200

    ss

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,14 +15,24 @@ class Authenticate extends Middleware
      * @return string|null
      */
    protected function redirectTo($request)
-{
+   {
        if (! $request->expectsJson()) {
           return route('login');
 
-
-        }
+         // return Route::post('api/auth/login', 'Api\UserControllers@login');
+       }
     }
 
 
+    public function handle($request, Closure $next)
+    {
+        if(!empty(trim($request->input('token')))){
 
+            $is_exists = User::where('id' , Auth::guard('api')->id())->exists();
+            if($is_exists){
+                return $next($request);
+            }
+        }
+        return response()->json('Invalid Token', 401);
+    }
 }
