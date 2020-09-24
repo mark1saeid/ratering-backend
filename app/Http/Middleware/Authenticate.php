3dd317fd2e67280f4ce07ac667778605d1889f4f
Your Name <you@example.com>
2020-09-24 09:21:21 +0200

    ss

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
+
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use Illuminate\Support\Facades\Route;
+
 
 class Authenticate extends Middleware
 {
@@ -18,21 +18,9 @@ class Authenticate extends Middleware
    {
        if (! $request->expectsJson()) {
           return route('login');
-
-         // return Route::post('api/auth/login', 'Api\UserControllers@login');
        }
+
     }
 
 
-    public function handle($request, Closure $next)
-    {
-        if(!empty(trim($request->input('token')))){
-
-            $is_exists = User::where('id' , Auth::guard('api')->id())->exists();
-            if($is_exists){
-                return $next($request);
-            }
-        }
-        return response()->json('Invalid Token', 401);
-    }
 }
