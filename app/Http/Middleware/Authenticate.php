b73da0bc8cdbd61e4e3ddd6175edf66cdd5bf953
Your Name <you@example.com>
2020-09-24 08:15:43 +0200

    ss

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -16,9 +16,9 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-          // return route('login');
+           return route('api/auth/login');
 
-           return Route::post('api/auth/login', 'Api\UserControllers@login');
+         //  return Route::post('api/auth/login', 'Api\UserControllers@login');
         }
     }
 }
