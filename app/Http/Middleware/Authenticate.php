b2c030201ad7233c78f0287fe2bba6236679200e
Your Name <you@example.com>
2020-09-25 11:05:22 +0200

    ss

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 
@@ -14,14 +15,17 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-   protected function redirectTo($request)
-   {
-       if (! $request->expectsJson()) {
-           return response()->json(['message' => 'Unauthenticated.']);
+  // protected function redirectTo($request)
+  // {
+   //    if (! $request->expectsJson()) {
+    //       return response()->json(['message' => 'Unauthenticated.']);
          //  route('login')
-       }
+   //    }
+  //  }
 
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return response()->json(['error' => 'Unauthenticated.'], 401);
     }
-
 
 }
