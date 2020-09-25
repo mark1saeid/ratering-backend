de737ce0167ba689889fa079b6b52afefc8d658b
Your Name <you@example.com>
2020-09-25 11:05:48 +0200

    ss

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,7 +12,8 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     * @param AuthenticationException $exception
      * @return string|null
      */
   // protected function redirectTo($request)
