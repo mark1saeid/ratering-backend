7b45e877daf569d5b262f6cf3ef5fcdb3e53a0e6
Your Name <you@example.com>
2020-09-25 11:31:10 +0200

    ss

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -6,7 +6,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\JWTAuth;
 
 
 
