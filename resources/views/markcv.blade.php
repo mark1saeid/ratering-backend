f35f18d69813f21962c9cd18bf633840b1cd0dd5
Your Name <you@example.com>
2020-10-14 21:00:11 +0200

    remove connection

--- resources/views/markcv.blade.php
+++ resources/views/markcv.blade.php
@@ -521,19 +521,6 @@
                                 <span class="comit"><i class="fa fa-quote-right"></i></span>
                             </div>
                         </div>
-                        <div class="testimonial-box">
-                            <div class="author-test">
-                                <img src="{{URL::asset('assets/img/testimonial-4.jpg')}}" alt="" class="rounded-circle b-shadow-a">
-                                <span class="author">Marta Socrate</span>
-                            </div>
-                            <div class="content-test">
-                                <p class="description lead">
-                                    Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Lorem ipsum dolor sit amet,
-                                    consectetur adipiscing elit.
-                                </p>
-                                <span class="comit"><i class="fa fa-quote-right"></i></span>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
