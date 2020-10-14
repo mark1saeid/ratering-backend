b24e1f66d3593dcb80d11dea8a4d6ae2ca392595
Your Name <you@example.com>
2020-10-14 19:01:14 +0200

    remove connection

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/marksaeid', function () {
+    return view('Mark/mark_home_cv');
+});
