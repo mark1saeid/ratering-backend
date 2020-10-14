0250f7d5d5a0d1291fb419d095a874c133e3e4e4
Your Name <you@example.com>
2020-10-14 23:08:44 +0200

    ss

--- resources/views/markcv.blade.php
+++ resources/views/markcv.blade.php
@@ -10,7 +10,7 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="{{URL::asset('assets/img/favicon.png')}}" rel="icon">
+    <link href="{{URL::asset('assets/img/rateringico.png')}}" rel="icon">
     <link href="{{URL::asset('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
     <!-- Vendor CSS Files -->
