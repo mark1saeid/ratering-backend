18e0bea46257611c73034461e648d196bfea4d06
Your Name <you@example.com>
2020-10-15 14:36:43 +0200

    ss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,20 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+    <link rel="icon" type="image/png" href="{{URL::asset('img/icons/favicon.ico')}}"/>
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('vendor/bootstrap/css/bootstrap.min.css')}}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('fonts/iconic/css/material-design-iconic-font.min.css')}}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('vendor/animate/animate.css')}}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('vendor/select2/select2.min.css')}}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="css/util.css">
-    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('css/util.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('css/main.css')}}">
     <!--===============================================================================================-->
 </head>
 <body>
@@ -115,17 +115,17 @@
 
 
 <!--===============================================================================================-->
-<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<script src="{{URL::asset('vendor/jquery/jquery-3.2.1.min.js')}}"></script>
 <!--===============================================================================================-->
-<script src="vendor/bootstrap/js/popper.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="{{URL::asset('vendor/bootstrap/js/popper.js')}}"></script>
+<script src="{{URL::asset('vendor/bootstrap/js/bootstrap.min.js')}}"></script>
 <!--===============================================================================================-->
-<script src="vendor/select2/select2.min.js"></script>
+<script src="{{URL::asset('vendor/select2/select2.min.js')}}"></script>
 <!--===============================================================================================-->
-<script src="vendor/countdowntime/moment.min.js"></script>
-<script src="vendor/countdowntime/moment-timezone.min.js"></script>
-<script src="vendor/countdowntime/moment-timezone-with-data.min.js"></script>
-<script src="vendor/countdowntime/countdowntime.js"></script>
+<script src="{{URL::asset('vendor/countdowntime/moment.min.js')}}"></script>
+<script src="{{URL::asset('vendor/countdowntime/moment-timezone.min.js')}}"></script>
+<script src="{{URL::asset('vendor/countdowntime/moment-timezone-with-data.min.js')}}"></script>
+<script src="{{URL::asset('vendor/countdowntime/countdowntime.js')}}"></script>
 <script>
     $('.cd100').countdown100({
         // Set Endtime here
@@ -142,14 +142,14 @@
     });
 </script>
 <!--===============================================================================================-->
-<script src="vendor/tilt/tilt.jquery.min.js"></script>
+<script src="{{URL::asset('vendor/tilt/tilt.jquery.min.js')}}"></script>
 <script >
     $('.js-tilt').tilt({
         scale: 1.1
     })
 </script>
 <!--===============================================================================================-->
-<script src="js/main.js"></script>
+<script src="{{URL::asset('js/jsload/main.js')}}"></script>
 
 </body>
 </html>
