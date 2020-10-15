bc800b7007132d2d639d5db09836718b29ab0255
Your Name <you@example.com>
2020-10-15 16:10:52 +0200

    ss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,7 +40,7 @@
 
         <div class="flex-w flex-c-m cd100 p-b-33">
             <div class="flex-col-c-m size2 bor1 m-l-15 m-r-15 m-b-20">
-                <span class="l2-txt1 p-b-9 days">0</span>
+                <span class="l2-txt1 p-b-9 days">60</span>
                 <span class="s2-txt1">Days</span>
             </div>
 
@@ -95,7 +95,7 @@
     $('.cd100').countdown100({
         endtimeYear: 0,
         endtimeMonth: 0,
-        endtimeDate: 60,
+        endtimeDate: 0,
         endtimeHours: 0,
         endtimeMinutes: 0,
         endtimeSeconds: 0,
