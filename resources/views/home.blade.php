ec293fa8e4ec2e8c14dac91a7f42592e87fbcefd
Your Name <you@example.com>
2020-10-15 13:12:41 +0200

    ss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,15 +11,15 @@
     <title>Coming Soon - Start Bootstrap Theme</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="{{asset('homeassets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Merriweather:300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
-    <link href="{{asset('homeassets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
 
     <!-- Custom styles for this template -->
-    <link href="{{asset('homeassets/css/coming-soon.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/coming-soon.min.css')}}" rel="stylesheet">
 
 </head>
 
@@ -27,7 +27,7 @@
 
 <div class="overlay"></div>
 <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
-    <source src="{{asset('homeassets/mp4/bg.mp4')}}'" type="video/mp4">
+    <source src="{{asset('mp4/bg.mp4')}}'" type="video/mp4">
 </video>
 
 <div class="masthead">
@@ -72,11 +72,11 @@
 </div>
 
 <!-- Bootstrap core JavaScript -->
-<script src="assets/homeassets/vendor/jquery/jquery.min.js'"></script>
-<script src="{{asset('homeassets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('vendor/jquery/jquery.min.js')}}'"></script>
+<script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
 <!-- Custom scripts for this template -->
-<script src="{{asset('homeassets/js/coming-soon.min.js')}}"></script>
+<script src="{{asset('js/coming-soon.min.js')}}"></script>
 
 </body>
 
