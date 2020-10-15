3b5a6f42cd90b66b3f829e5eaa5ab117d543a998
Your Name <you@example.com>
2020-10-15 14:30:08 +0200

    ss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,121 +1,155 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Coming Soon 2</title>
+    <title>Coming Soon 4</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="{{asset('img/icons/favicon.ico')}}"/>
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{asset('fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{asset('vendor/animate/animate.css')}}">
+    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{asset('vendor/select2/select2.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{asset('css/util.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}">
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
     <!--===============================================================================================-->
 </head>
 <body>
 
-<!--  -->
-<div class="simpleslide100">
-    <div class="simpleslide100-item bg-img1" style="background-image: url('public/assets/img/bg01.jpg');"></div>
-    <div class="simpleslide100-item bg-img1" style="background-image: url('public/assets/img/bg02.jpg');"></div>
-    <div class="simpleslide100-item bg-img1" style="background-image: url('public/assets/img/bg03.jpg');"></div>
-</div>
 
-<div class="size1 overlay1">
-    <!--  -->
-    <div class="size1 flex-col-c-m p-l-15 p-r-15 p-t-50 p-b-50">
-        <h3 class="l1-txt1 txt-center p-b-25">
+<div class="bg-g1 size1 flex-w flex-col-c-sb p-l-15 p-r-15 p-t-55 p-b-35 respon1">
+    <span></span>
+    <div class="flex-col-c p-t-50 p-b-50">
+        <h3 class="l1-txt1 txt-center p-b-10">
             Coming Soon
         </h3>
 
-        <p class="m2-txt1 txt-center p-b-48">
-            Our website is under construction, follow us for update now!
+        <p class="txt-center l1-txt2 p-b-60">
+            Our website is under construction
         </p>
 
-        <div class="flex-w flex-c-m cd100 p-b-33">
-            <div class="flex-col-c-m size2 bor1 m-l-15 m-r-15 m-b-20">
-                <span class="l2-txt1 p-b-9 days">35</span>
-                <span class="s2-txt1">Days</span>
+        <div class="flex-w flex-c cd100 p-b-82">
+            <div class="flex-col-c-m size2 how-countdown">
+                <span class="l1-txt3 p-b-9 days">35</span>
+                <span class="s1-txt1">Days</span>
             </div>
 
-            <div class="flex-col-c-m size2 bor1 m-l-15 m-r-15 m-b-20">
-                <span class="l2-txt1 p-b-9 hours">17</span>
-                <span class="s2-txt1">Hours</span>
+            <div class="flex-col-c-m size2 how-countdown">
+                <span class="l1-txt3 p-b-9 hours">17</span>
+                <span class="s1-txt1">Hours</span>
             </div>
 
-            <div class="flex-col-c-m size2 bor1 m-l-15 m-r-15 m-b-20">
-                <span class="l2-txt1 p-b-9 minutes">50</span>
-                <span class="s2-txt1">Minutes</span>
+            <div class="flex-col-c-m size2 how-countdown">
+                <span class="l1-txt3 p-b-9 minutes">50</span>
+                <span class="s1-txt1">Minutes</span>
             </div>
 
-            <div class="flex-col-c-m size2 bor1 m-l-15 m-r-15 m-b-20">
-                <span class="l2-txt1 p-b-9 seconds">39</span>
-                <span class="s2-txt1">Seconds</span>
+            <div class="flex-col-c-m size2 how-countdown">
+                <span class="l1-txt3 p-b-9 seconds">39</span>
+                <span class="s1-txt1">Seconds</span>
             </div>
         </div>
 
-        <form class="w-full flex-w flex-c-m validate-form">
+        <button class="flex-c-m s1-txt2 size3 how-btn"  data-toggle="modal" data-target="#subscribe">
+            Follow us for update now!
+        </button>
+    </div>
 
-            <div class="wrap-input100 validate-input where1" data-validate = "Valid email is required: ex@abc.xyz">
-                <input class="input100 placeholder0 s2-txt2" type="text" name="email" placeholder="Enter Email Address">
-                <span class="focus-input100"></span>
-            </div>
+    <span class="s1-txt3 txt-center">
+			@ 2017 Coming Soon Template. Designed by Colorlib
+		</span>
 
+</div>
 
-            <button class="flex-c-m size3 s2-txt3 how-btn1 trans-04 where1">
-                Subscribe
+<!-- Modal Login -->
+<div class="modal fade" id="subscribe" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog" role="document" data-dismiss="modal">
+        <div class="modal-subscribe where1-parent bg0 bor2 size4 p-t-54 p-b-100 p-l-15 p-r-15">
+            <button class="btn-close-modal how-btn2 fs-26 where1 trans-04">
+                <i class="zmdi zmdi-close"></i>
             </button>
-        </form>
+
+            <div class="wsize1 m-lr-auto">
+                <h3 class="m1-txt1 txt-center p-b-36">
+                    <span class="bor1 p-b-6">Subscribe</span>
+                </h3>
+
+                <p class="m1-txt2 txt-center p-b-40">
+                    Follow us for update now!
+                </p>
+
+                <form class="contact100-form validate-form">
+                    <div class="wrap-input100 m-b-10 validate-input" data-validate = "Name is required">
+                        <input class="s1-txt4 placeholder0 input100" type="text" name="name" placeholder="Name">
+                        <span class="focus-input100"></span>
+                    </div>
+
+                    <div class="wrap-input100 m-b-20 validate-input" data-validate = "Email is required: ex@abc.xyz">
+                        <input class="s1-txt4 placeholder0 input100" type="text" name="email" placeholder="Email">
+                        <span class="focus-input100"></span>
+                    </div>
+
+                    <div class="w-full">
+                        <button class="flex-c-m s1-txt2 size5 how-btn1 trans-04">
+                            Get Updates
+                        </button>
+                    </div>
+                </form>
+
+                <p class="s1-txt5 txt-center wsize2 m-lr-auto p-t-20">
+                    And don’t worry, we hate spam too! You can unsubcribe at anytime.
+                </p>
+            </div>
+        </div>
+
     </div>
 </div>
 
 
 
-
-
 <!--===============================================================================================-->
-<script src="{{asset('vendor/jquery/jquery-3.2.1.min.js')}}"></script>
+<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-<script src="{{asset('vendor/bootstrap/js/popper.js')}}"></script>
-<script src="{{asset('vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="vendor/bootstrap/js/popper.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-<script src="{{asset('vendor/select2/select2.min.js')}}"></script>
+<script src="vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-<script src="{{asset('vendor/countdowntime/moment.min.js')}}"></script>
-<script src="{{asset('vendor/countdowntime/moment-timezone.min.js')}}"></script>
-<script src="{{asset('vendor/countdowntime/moment-timezone-with-data.min.js')}}"></script>
-<script src="{{asset('vendor/countdowntime/countdowntime.js')}}"></script>
+<script src="vendor/countdowntime/moment.min.js"></script>
+<script src="vendor/countdowntime/moment-timezone.min.js"></script>
+<script src="vendor/countdowntime/moment-timezone-with-data.min.js"></script>
+<script src="vendor/countdowntime/countdowntime.js"></script>
 <script>
     $('.cd100').countdown100({
-        /*Set Endtime here*/
-        /*Endtime must be > current time*/
+        // Set Endtime here
+        // Endtime must be > current time
         endtimeYear: 0,
         endtimeMonth: 0,
         endtimeDate: 35,
         endtimeHours: 18,
         endtimeMinutes: 0,
         endtimeSeconds: 0,
-        timeZone: "Africa/Cairo"
-        // ex:  timeZone: "America/New_York"
-        //go to " http://momentjs.com/timezone/ " to get timezone
+        timeZone: ""
+        // ex:  timeZone: "America/New_York", can be empty
+        // go to " http://momentjs.com/timezone/ " to get timezone
     });
 </script>
 <!--===============================================================================================-->
-<script src="{{asset('vendor/tilt/tilt.jquery.min.js')}}"></script>
+<script src="vendor/tilt/tilt.jquery.min.js"></script>
 <script >
     $('.js-tilt').tilt({
         scale: 1.1
     })
 </script>
 <!--===============================================================================================-->
-<script src="{{asset('js/main.js')}}"></script>
+<script src="js/main.js"></script>
 
 </body>
 </html>
