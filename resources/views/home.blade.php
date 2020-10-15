bc8431f8054b1914202602ecfae39eb2dac33c87
Your Name <you@example.com>
2020-10-15 12:37:11 +0200

    ss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,68 +1,83 @@
 <!DOCTYPE html>
-<html lang="en-US">
+<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Coming Soon Bootstrap 4 Template Demo</title>
-    <meta name="description" content="Demo of A Free Coming Soon Bootstrap 4 Template by TemplateFlip.com."/>
-    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylesheet">
-    <link href="{{URL::asset('homeassets/css/font-awesome.min.css')}}" rel="stylesheet">
-    <link href="{{URL::asset('homeassets/styles/main.css')}}" rel="stylesheet">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Coming Soon - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{URL::asset('homeassets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+    <link href="{{URL::asset('homeassets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="{{URL::asset('homeassets/css/coming-soon.min.css')}}" rel="stylesheet">
+
 </head>
-<body id="top">
-<div class="site-wrapper">
-    <div class="site-wrapper-inner">
-        <div class="cover-container">
-            <div class="masthead clearfix">
-                <div class="inner">
-                    <h3 class="masthead-brand">Coming Soon</h3>
-                    <nav class="nav nav-masthead">
-                        <a class="nav-link nav-social" href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                        <a class="nav-link nav-social" href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                        <a class="nav-link nav-social" href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-                        <a class="nav-link nav-social" href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                    </nav>
-                </div>
-            </div><br>      <div class="inner cover">
-                <h1 class="cover-heading">The adventure Begins</h1>
-                <p class="lead cover-copy">Hold tight as we get our working robots together and produce the most astonishing product ever.</p>
-                <p class="lead"><button type="button" class="btn btn-lg btn-default btn-notify" data-toggle="modal" data-target="#subscribeModal">Notify Me</button></p>
-            </div>
-            <div class="mastfoot">
-                <div class="inner">
-                    <p>&copy; RateRing. Design: <a href="#" target="_blank">Mark</a>.</p>
-                </div>
-            </div>
-            <div class="modal fade" id="subscribeModal" tabindex="-1" role="dialog" aria-labelledby="subscribeModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="subscribeModalLabel">Get Notified on Launch:</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <form>
-                                <div class="form-group">
-                                    <label for="recipient-name" class="form-control-label">Enter you e-mail to get notified when we launch</label>
-                                    <input type="text" class="form-control" id="recipient-name" placeholder="your-name@example.com">
-                                </div>
-                            </form>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-default">Subscribe</button>
+
+<body>
+
+<div class="overlay"></div>
+<video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+    <source src="{{URL::asset('homeassets/mp4/bg.mp4')}}" type="video/mp4">
+</video>
+
+<div class="masthead">
+    <div class="masthead-bg"></div>
+    <div class="container h-100">
+        <div class="row h-100">
+            <div class="col-12 my-auto">
+                <div class="masthead-content text-white py-5 py-md-0">
+                    <h1 class="mb-3">Coming Soon!</h1>
+                    <p class="mb-5">We're working hard to finish the development of this site. Our target launch date is
+                        <strong>January 2019</strong>! Sign up for updates using the form below!</p>
+                    <div class="input-group input-group-newsletter">
+                        <input type="email" class="form-control" placeholder="Enter email..." aria-label="Enter email..." aria-describedby="submit-button">
+                        <div class="input-group-append">
+                            <button class="btn btn-secondary" type="button" id="submit-button">Notify Me!</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
-    <script src="{{URL::asset('homeassets/scripts/main.js')}}"></script>
+</div>
+
+<div class="social-icons">
+    <ul class="list-unstyled text-center mb-0">
+        <li class="list-unstyled-item">
+            <a href="#">
+                <i class="fab fa-twitter"></i>
+            </a>
+        </li>
+        <li class="list-unstyled-item">
+            <a href="#">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+        </li>
+        <li class="list-unstyled-item">
+            <a href="#">
+                <i class="fab fa-instagram"></i>
+            </a>
+        </li>
+    </ul>
+</div>
+
+<!-- Bootstrap core JavaScript -->
+<script src="{{URL::asset('homeassets/vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{URL::asset('homeassets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+<!-- Custom scripts for this template -->
+<script src="{{URL::asset('homeassets/js/coming-soon.min.js')}}"></script>
+
 </body>
+
 </html>
