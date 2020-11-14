7017fe420210ff61849459866c20e50eebfc650e
marksaeid <marksaeid10@gmail.com>
2020-11-14 20:48:01 +0200

    ss

--- app/Http/Controllers/Api/Postcontrollers.php
+++ app/Http/Controllers/Api/Postcontrollers.php
@@ -37,7 +37,7 @@ class Postcontrollers extends Controller
 
 
     function trend(){
-        $post = Post::all()->where('impression_24','>','0')->sortByDesc('impression_24');
+        $post = Post::all()->where('impression_24','>','0');
        return $post ;
 
     }
