e576480744338a868d71d5dd1af7e32dff95dd86
marksaeid <marksaeid10@gmail.com>
2020-11-14 18:40:27 +0200

    ss

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 
-
+        resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
 
         Schema::defaultStringLength(191);
     }
