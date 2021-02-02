b234463a116dd03fe2839e9bfa423baae4887f58
marksaeid <marksaeid10@gmail.com>
2021-02-02 03:31:58 +0200

    ss

--- app/Http/Controllers/Api/UserControllers.php
+++ app/Http/Controllers/Api/UserControllers.php
@@ -100,7 +100,7 @@ use GeneralTrait;
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth('api')->factory()->getTTL() * 60,
+           // 'expires_in' => auth('api')->factory()->getTTL() * 60,
             'user' => auth()->user()
         ]);
     }
